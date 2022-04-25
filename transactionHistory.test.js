431db1823b5ef6dca334e21044a839b7a7727802
Harry <harrydawson90@hotmail.co.uk>
2022-04-25 16:20:19 +0100

    refactor test - removed dependency on Transaction class

--- transactionHistory.test.js
+++ transactionHistory.test.js
@@ -1,10 +1,16 @@
 const History = require("./transactionHistory");
-const Transaction = require("./transaction");
+
+const transaction = {
+  type: 'deposit',
+  difference: 1000,
+  date: 'today'
+}
 
 describe("TransactionHistory", () => {
   it("can add a transaction", () => {
     const transactionHistory = new History()
-    transactionHistory.addTransaction(new Transaction('deposit', 1000))
+    transactionHistory.addTransaction(transaction)
+    console.log(transaction)
     expect(transactionHistory.transactions).toBeArray;
     expect(transactionHistory.transactions[0]).toEqual(
       expect.objectContaining({
