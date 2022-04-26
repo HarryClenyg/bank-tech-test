da78133a8b8acd6dc7b7a7db68809864af75023b
Harry <harrydawson90@hotmail.co.uk>
2022-04-26 15:48:30 +0100

    updated statement method to print string instead of array

--- bankAccount.js
+++ bankAccount.js
@@ -1,15 +1,15 @@
 class BankAccount {
   constructor (balance = 0) {
-    this.statement = ['date || credit || debit || balance'];
+    this.statement = 'date || credit || debit || balance';
     this.balance = balance
   }
 
-  createStatement(history) {
+  printStatement(history) {
     history.forEach(transaction => {
       if(transaction.type === 'deposit'){
-        this.statement.push(`${transaction.date} || ${transaction.amount.toFixed(2)} ||  || ${(this.balance += transaction.amount).toFixed(2)}`);
+        this.statement += `\n${transaction.date} || ${transaction.amount.toFixed(2)} ||  || ${(this.balance += transaction.amount).toFixed(2)}`;
       } else if (transaction.type === 'withdraw'){
-        this.statement.push(`${transaction.date} ||  || ${transaction.amount.toFixed(2)} || ${(this.balance -= transaction.amount).toFixed(2)}`);
+        this.statement += `\n${transaction.date} ||  || ${transaction.amount.toFixed(2)} || ${(this.balance -= transaction.amount).toFixed(2)}`;
       }
     })
     return this.statement
